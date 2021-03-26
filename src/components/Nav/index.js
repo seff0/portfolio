11e8c37414ebba5c6fa6ae34e77a8aedf43862c8
seff0 <76668281+seff0@users.noreply.github.com>
2021-03-25 19:52:03 -0600

    create nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function Nav() {
+	return (
+		<div>
+			<nav className="navbar navbar-expand-md">
+				<div className="container-fluid">
+					<a className="navbar-brand" href="/">
+						Seth Hill
+					</a>
+					<button
+						className="navbar-toggler custom-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+							<li className="nav-item">
+								<a className="nav-link" aria-current="page" href="/">
+									About Me
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="/portfolio">
+									Portfolio
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="/contact">
+									Contact
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	);
+}
+
+export default Nav;
