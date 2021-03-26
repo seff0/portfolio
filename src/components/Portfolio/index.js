d04b24be3019e2b613fc984f855b2f0d07e24226
seff0 <76668281+seff0@users.noreply.github.com>
2021-03-25 20:01:46 -0600

    create project page wrapper

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Project from "../Project";
+
+function Portfolio() {
+	return (
+		<main class="container">
+			<section class="row">
+				<div class="col-lg-8">
+					<h1>Portfolio</h1>
+				</div>
+			</section>
+			<Project />
+		</main>
+	);
+}
+
+export default Portfolio;
