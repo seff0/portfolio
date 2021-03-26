7f3d2e9079034505d976da54d29602a930f3082d
seff0 <76668281+seff0@users.noreply.github.com>
2021-03-25 19:55:50 -0600

    create about component

---
+++ src/components/About/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function About() {
+	return (
+		<main class="container">
+			<section class="row">
+				<div class="col-lg-8">
+					<h1>About Me</h1>
+				</div>
+			</section>
+			<section class="row">
+				<div class="col-lg-8">
+					<img src="./assets/headshot.jpg" alt="Seth Hill" />
+					<p>
+						My name is Seth Hill, aspiring full stack developer from Sterling,
+						CO. I've been working in transportation/delivery services for a few
+						years now and I'm ready to transition to a career in a field I feel
+						passionate about. I've always had a love for computers. I love to
+						build and upgrade my systems as much as I love gaming on them.
+					</p>
+					<p>
+						I'll be updating this page as my portfolio grows to showcase my most
+						recent work!
+					</p>
+				</div>
+			</section>
+		</main>
+	);
+}
+
+export default About;
