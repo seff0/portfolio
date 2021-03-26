79109e7d44335571bca6b7760e9129bf36aee4f6
seff0 <76668281+seff0@users.noreply.github.com>
2021-03-25 20:09:45 -0600

    create project component

---
+++ src/components/Project/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Project(props) {
+	return (
+		<div className="col-lg-6">
+			<a href={props.href}>
+				<img
+					src={props.screen}
+					className="float-start screen"
+					alt={props.title}
+				/>
+			</a>
+			<p>
+				{props.title}
+				<br />
+				{props.description}
+				<br />
+				Role: {props.role}
+				<br />
+				Tools: {props.tools}
+			</p>
+			<a href={props.href} className="personal-link" target="_blank">
+				View on GitHub
+			</a>
+		</div>
+	);
+}
+
+export default Project;
